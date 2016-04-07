130ee4a181feea868bf24e8acbdcb419f923f73b
A M Leszkiewicz <amleszk@gmail.com>
2016-04-07 18:49:11 -0400

    Initial commit

--- RxPasscode/ViewController.swift
+++ RxPasscode/ViewController.swift
@@ -10,16 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var passcodePresenter: PasscodePresenter!
+    
+    override func viewDidAppear(animated: Bool) {
+        self.view.backgroundColor = UIColor.blueColor()
+        passcodePresenter = PasscodePresenter()
+        passcodePresenter.presentInKeyWindow()
+        
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            self.passcodePresenter.dismiss()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
 }
 
+class PasscodePresenter {
+    
+    let presentedWindow: UIWindow
+    
+    private lazy var passcodeLockWindow: UIWindow = {
+        let window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window.windowLevel = 0
+        window.makeKeyAndVisible()
+        return window
+    }()
+    
+    init() {
+        presentedWindow = UIApplication.sharedApplication().keyWindow!
+    }
+    
+    func presentInKeyWindow() {
+        presentedWindow.windowLevel = 1
+        presentedWindow.endEditing(true)
+        passcodeLockWindow.windowLevel = 2
+        passcodeLockWindow.makeKeyAndVisible()
+        passcodeLockWindow.rootViewController = PasscodeLockViewController()
+        
+    }
+    
+    func dismiss() {
+        passcodeLockWindow.hidden = true
+        presentedWindow.becomeKeyWindow()
+    }
+}
+
+class PasscodeLockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
+    }
+    
+}
